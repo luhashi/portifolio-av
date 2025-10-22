60250cc0ead3c7cc4e002b22ac7a2bad9b641d4b
lucas hashimoto <lucas.hashien@gmail.com>
2025-10-22 09:57:11 +0000

    vamos usar essa foto no sobre mim https://res.cloudinary.com/dolqcmwlv/i

--- next.config.ts
+++ next.config.ts
@@ -39,6 +39,12 @@ const nextConfig: NextConfig = {
         hostname: 'storage.googleapis.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
